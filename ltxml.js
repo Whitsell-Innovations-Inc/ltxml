b0783e788f978e87f180e70efbf07900778d0500
Sean Whitsell <66265823+seanwhitsellAtWork@users.noreply.github.com>
2023-03-30 12:42:33 -0400

    Add dependency on xmldom

--- ltxml.js
+++ ltxml.js
@@ -315,6 +315,7 @@ Email: eric@ericwhite.com
 var Enumerable = require('linq');
 var Ltxml;
 
+const { DOMParser } = require('xmldom');
 var parseXml, Functions, Ltxml, addContentThatCanContainEntities,
     serializeAttributeContent, annotateRootForNamespaces,
     prefixCounter, entityCodePoints, entities,
@@ -363,22 +364,8 @@ parseXml = function (xmlStr) {
         xmlStr = xmlStr.substring(1);
     }
 
-    if (typeof Ltxml.DOMParser !== "undefined") {
-        domParser = (new Ltxml.DOMParser()).parseFromString(xmlStr, "application/xml");
-        return domParser;
-    }
-    else if (typeof window.DOMParser !== "undefined") {
-        domParser = (new window.DOMParser()).parseFromString(xmlStr, "application/xml");
-        return domParser;
-    } else if (typeof window.ActiveXObject !== "undefined" &&
-            new window.ActiveXObject("Microsoft.XMLDOM")) {
-        var xmlDoc = new window.ActiveXObject("Microsoft.XMLDOM");
-        xmlDoc.async = "false";
-        xmlDoc.loadXML(xmlStr);
-        return xmlDoc;
-    } else {
-        throw new Error("No XML parser found");
-    }
+    domParser = new DOMParser().parseFromString(xmlStr, "application/xml");
+    return domParser;
 };
 /*ignore jslint end*/
 
